291de2bc77c44a7e8d4630943fffa82c197e17ee
xcaptainvancity <39244724+xcaptainvancity@users.noreply.github.com>
2019-07-06 14:13:37 +0700

    Update login.h
    * Perubahan default folder penyimpanan data user.
    * Proses enkripsi pada password yang disimpan. Sehingga password tidak dapat diketahui orang lain.

--- login.h
+++ login.h
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <stdlib.h>
 #include "menu.h"
 
 using namespace std;
@@ -9,16 +12,28 @@ bool IsLoggedIn()
   cout << "   Username : "; cin >> username;
   cout << "   Password : "; cin >> password;
 
-  ifstream read("E:\\FP\\USER\\" + username + ".txt");
+  ifstream read("C:\\" + username + ".txt");
   getline(read, un);
   getline(read, pw);
+  string encrypHexa = "";
+        int keyItr = 0;
+        string key = "5";
+        stringstream res;
+        for (int i = 0; i < password.length(); i++){
+            int temp = password[i] ^ key[keyItr];
 
-  if (un == username && pw == password)
+            res << hex << setfill('0') << std::setw(2) << (int)temp;
+            keyItr++;
+            if (keyItr >= key.length()){
+                keyItr = 0;
+            }
+        }
+
+        res >> encrypHexa;
+  if (un == username && pw == encrypHexa)
   {
     return true;
   } else {
     return false;
   }
 }
-
-
